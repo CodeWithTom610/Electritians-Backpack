41b3b1c7fd329028373b99735d6ac18695afe258
Tom Walla <tom.walla@icloud.com>
2024-10-04 14:11:30 +0000

    Creating Database

--- app.py
+++ app.py
@@ -2,9 +2,10 @@
 #############################               Packages            ###############################################
 ###############################################################################################################
 
-from    flask              import      *
-from    flask_wtf          import      *
-from    flask_bootstrap    import      Bootstrap5
+from    flask                                   import      *
+from    flask_wtf                               import      *
+from    flask_bootstrap                         import      Bootstrap5
+from    flask_sqlalchemy                        import      SQLAlchemy
 
 
 ###############################################################################################################
@@ -12,44 +13,68 @@ from    flask_bootstrap    import      Bootstrap5
 ###############################################################################################################
 
 #   Initialize Flask App        ###############################################################################
-app                                             =       Flask(__name__)
+app                                             =           Flask(__name__)
 
 #   Initialize Bootstrap App    ###############################################################################
-bootstrap                                       =       Bootstrap5(app)
+bootstrap                                       =           Bootstrap5(app)
 
 #   Configure Flask App         ##############################################################################
 
-app.config["SECRET_KEY"]                        =       "7cd870fd19e3899b23e0eaafb97e094494b91d9957815a1"
-app.config["IMAGE_FOLDER"]                      =       "./static/IMG/"
+app.config["SECRET_KEY"]                        =           "7cd870fd19e3899b23e0eaafb97e094494b91d9957815a1"
+app.config["IMAGE_FOLDER"]                      =           "./static/IMG/"
+app.config["SQLALCHEMY_DATABASE_URI"]           =           "sqlite:///database.db"
 
+#   Initialize Database         ##############################################################################
+db                                              =           SQLAlchemy()
+db.                                             init_app    (app)
 
 ###############################################################################################################
 ############################                Routes                #############################################
 ###############################################################################################################
 
 @app.route('/')
-def             index():
+def             news():
     return      render_template(
-                                                        'index.html',
-                title                          =        "Home | EBT-Backpack")
+                                                            'index.html',
+                title                           =           "Home | EBT-Backpack")
 
 @app.route('/tools-complete')
 def             tools_complete():
     return      render_template(
-                                                        'alle_tools.html',
-                title                          =        "Alle Tools | EBT-Backpack")
+                                                            'alle_tools.html',
+                title                           =           "Alle Tools | EBT-Backpack")
 
 @app.route('/admin')
 def             admin_login():
     return      render_template(
-                                                        'admin_login.html', 
-                title                          =        "Login | EBT-Backpack")
+                                                            'admin_login.html', 
+                title                           =           "Login | EBT-Backpack")
 
+
+###############################################################################################################
+############################           Database Models         ################################################
+###############################################################################################################
+class New_Card                                              (db.Model):
+    __tablename__                               =           "NewsCards"
+    id                                          =            db.Column(
+                                                             db.Integer,
+                                                             primary_key   =   True)
+    header                                      =            db.Column(
+                                                             db.String,
+                                                             unique=True,
+                                                             nullable=False)
+    content                                     =            db.Column(
+                                                             db.String)
+    author                                      =            db.Column(
+                                                             db.String)
+    imagepath                                   =            db.Column(
+                                                             db.String)
 
 ###############################################################################################################
 ############################              Run Dialog              #############################################
 ###############################################################################################################
+with                                             app.        app_context():
+    db.                                                      create_all()
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
- 
